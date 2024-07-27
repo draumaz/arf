9289197e145da4dc59d9430bc5e2b85fb5de2f6e
Emma Anderson <workeandrs@gmail.com>
2024-07-27 13:58:48 -0700

    implement magisk installer

--- arf.sh
+++ arf.sh
@@ -1,39 +1,83 @@
 #!/bin/bash -e
 
-# an automated ROM installer
-# runtime dependencies: adb, fastboot, payload-dumper-go, unzip
-# device requirements: bootloader unlocked, USB debugging enabled
+flash_rom() {
 
-case "${1}" in "")
-  printf "${0} [path to rom]\n"
-  exit ;;
+  if touch /tmp > /dev/null 2>&1; then mkdir -p /tmp/arf && cd /tmp/arf; fi
+
+  unzip "${1}" payload.bin
+
+  payload-dumper-go \
+    -partitions boot,vendor_boot,dtbo \
+    -output "${PWD}" \
+      payload.bin
+
+  rm -fv payload.bin
+
+  if adb shell ':' > /dev/null 2>&1; then
+    echo "rebooting to bootloader"
+    adb reboot bootloader && fastboot -w
+  else
+    fastboot -w
+  fi
+
+  for PART in boot vendor_boot dtbo; do
+    fastboot flash "${PART}" "${PART}.img"
+  done
+
+  fastboot reboot-recovery
+
+  sleep 10
+  echo "now, apply update from adb."
+  sleep 5
+
+  # Apply ROM to target device
+  until adb sideload "${1}"; do sleep 5; done
+
+}
+
+proc_magisk() {
+
+  if adb shell "test -e /dev/block/by-name/init_boot_a"; then
+    IMG="init_boot"; else IMG="boot"
+  fi
+
+  MAGISKVER="27.0"
+  MAGISKURL="https://github.com/topjohnwu/Magisk/releases/download/v${MAGISKVER}/Magisk-v${MAGISKVER}.apk"
+
+  test -e "./Magisk-v${MAGISKVER}.apk" || {
+    printf "downloading magisk...\n"
+    curl -fLO "${MAGISKURL}"
+  }
+
+  # Retrieve $IMG.img for currently running ROM for patching
+  unzip "${2}" payload.bin || exit 1
+  payload-dumper-go -partitions ${IMG} -output "${PWD}" payload.bin || exit 1
+  adb push "${IMG}.img" "/sdcard/Download/"
+
+  # Install and open Magisk on target device
+  adb install "`find . -name \*Magisk\*apk\* | tail -1`"
+  adb shell "monkey -p com.topjohnwu.magisk 1"
+
+  printf "\npatch /sdcard/Download/${IMG}.img in Magisk and press enter.\n"
+  read
+
+  # ls -atr sorts newest at the bottom; tail that to get the right file
+  MAGISKIMG="`adb shell ls -atr /sdcard/Download | grep -i magisk | tail -1`"
+
+  adb pull "/sdcard/Download/${MAGISKIMG}"
+  adb shell "rm -f /sdcard/Download/${MAGISKIMG} /sdcard/Download/${IMG}.img"
+  adb reboot bootloader
+
+  for PART in a b; do fastboot flash "${IMG}_${PART}" "${MAGISKIMG}"; done
+
+  fastboot reboot
+
+}
+
+case "${1}" in
+  --magisk|-m) proc_magisk "${@}" ;;
+  *.zip) flash_rom "${@}" ;;
+  *) echo "${0} [path to rom]" ;;
 esac
 
-if touch /tmp > /dev/null 2>&1; then cd /tmp; fi
-
-unzip "${1}" payload.bin
-
-payload-dumper-go \
-  -partitions boot,vendor_boot,dtbo \
-  -output "${PWD}" \
-    payload.bin
-
-rm -fv payload.bin
-
-if adb shell ':' > /dev/null 2>&1; then
-  echo "rebooting to bootloader"
-  adb reboot bootloader && fastboot -w
-else
-  fastboot -w
-fi
-
-for PART in boot vendor_boot dtbo; do
-  fastboot flash "${PART}" "${PART}.img"
-done
-
-fastboot reboot-recovery
-
-# Apply ROM to target device
-until adb sideload "${1}"; do sleep 5; done
-
-rm -fv *.bin *.img
+rm -fv *.{img,apk,bin}
