93d491f543860ec98fd0898e9d14338e77c8b153
Emma Anderson <workeandrs@gmail.com>
2024-07-28 05:07:05 -0700

    clearer messaging

--- arf.sh
+++ arf.sh
@@ -1,6 +1,19 @@
 #!/bin/bash -e
 
+rom_validity_check() {
+  if test -e "${1}"; then
+    if unzip -l "${1}" | grep payload.bin > /dev/null 2>&1; then
+      true
+    fi
+  else
+    printf "[${1} doesn't appear to be a valid ROM, exiting.]\n"
+    exit 1
+  fi
+}
+
 flash_rom() {
+
+  rom_validity_check "${1}"
 
   unzip "${1}" payload.bin
 
@@ -12,7 +25,7 @@ flash_rom() {
   rm -fv payload.bin
 
   if adb shell ':' > /dev/null 2>&1; then
-    echo "rebooting to bootloader"
+    printf "[rebooting to bootloader...]\n"
     adb reboot bootloader && fastboot -w
   else
     fastboot -w
@@ -25,7 +38,7 @@ flash_rom() {
   fastboot reboot-recovery
 
   sleep 10
-  echo "\n[now, apply update from adb]\n"
+  printf "[now, apply update from adb.]\n"
   sleep 5
 
   # Apply ROM to target device
@@ -35,6 +48,8 @@ flash_rom() {
 
 proc_magisk() {
 
+  rom_validity_check "${2}"
+
   if adb shell "test -e /dev/block/by-name/init_boot_a"; then
     IMG="init_boot"; else IMG="boot"
   fi
@@ -43,7 +58,7 @@ proc_magisk() {
   MAGISKURL="https://github.com/topjohnwu/Magisk/releases/download/v${MAGISKVER}/Magisk-v${MAGISKVER}.apk"
 
   test -e "./Magisk-v${MAGISKVER}.apk" || {
-    printf "\n[downloading magisk]\n"
+    printf "[downloading magisk...]\n"
     curl -fLO "${MAGISKURL}"
   }
 
@@ -56,7 +71,7 @@ proc_magisk() {
   adb install "`find . -name \*Magisk\*apk\* | tail -1`"
   adb shell "monkey -p com.topjohnwu.magisk 1"
 
-  printf "\n[patch /sdcard/Download/${IMG}.img in Magisk and press enter]\n"
+  printf "\n[patch /sdcard/Download/${IMG}.img in Magisk and press enter.]\n"
   read
 
   # ls -atr sorts newest at the bottom; tail that to get the right file
